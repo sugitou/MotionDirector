d9e731b489d8b5130e582465cac09475fe24504a
sugitou <rs02358@surrey.ac.uk>
2025-04-27 17:54:18 +0100

    Add seed

--- run_inference.sh
+++ run_inference.sh
@@ -4,5 +4,6 @@ python3 MotionDirector_inference.py \
   --model ./models/zeroscope_v2_576w/ \
   --prompt "A tank is running on the moon." \
   --checkpoint_folder ./outputs/myTrain/train_2025-04-27T17-02-47 \
-  --checkpoint_index 50 \
-  --noise_prior 0.5
+  --checkpoint_index 150 \
+  --noise_prior 0.5 \
+  --seed 8808231
