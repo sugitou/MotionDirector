39d7dc8e056f3ac2f5e4f4232dcaf0dda30e3a05
sugitou <rs02358@surrey.ac.uk>
2025-04-30 01:41:27 +0100

    Create video for interim review

--- run_inference.sh
+++ run_inference.sh
@@ -2,8 +2,8 @@
 
 python3 MotionDirector_inference.py \
   --model ./models/zeroscope_v2_576w/ \
-  --prompt "A tank is running on the moon." \
-  --checkpoint_folder ./outputs/myTrain/train_2025-04-27T17-02-47 \
-  --checkpoint_index 150 \
-  --noise_prior 0.5 \
-  --seed 8808231
+  --prompt "A person is riding a bicycle in a garden." \
+  --checkpoint_folder ./outputs/myTrain/train_2025-04-27T15-59-59 \
+  --checkpoint_index 500 \
+  --noise_prior 0. \
+  --seed 7192280
