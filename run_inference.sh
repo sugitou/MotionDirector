345f537460db34ef844b18fd4f7e2d537798dc9e
sugitou <rs02358@surrey.ac.uk>
2025-07-14 16:52:38 +0100

    Try creating video for meeting

--- run_inference.sh
+++ run_inference.sh
@@ -2,8 +2,9 @@
 
 python MotionDirector_inference.py \
   --model ./models/zeroscope_v2_576w/ \
-  --prompt "A mokey is playing golf on the moon." \
-  --checkpoint_folder ./outputs/myTrain/train_ucf101_golf_8fps_A_person_is_playing_golf_400 \
+  --prompt "A monkey is playing golf in Times Square New York." \
+  --checkpoint_folder ./outputs/myTrain/train_ucf101_golf_8fps_A_person_is_playing_golf_on_the_grass \
   --checkpoint_index 400 \
   --noise_prior 0. \
-  --seed 785490
+  --seed 785490 \
+  --num-steps 100 \
