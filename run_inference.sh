e4819899eeeffcd14af0675d6ec7d86ae4a9e20b
sugitou <rs02358@surrey.ac.uk>
2025-08-07 22:07:49 +0100

    Fix some results

--- run_inference.sh
+++ run_inference.sh
@@ -2,14 +2,13 @@
 
 python MotionDirector_inference.py \
   --model ./models/zeroscope_v2_576w/ \
-  --prompt "An athletic person holds a steady handstand on a sandy beach under a few white cloud sky." \
-  --checkpoint_folder ./outputs/myTrain/train_ucf101_HandstandWalking-short \
+  --prompt "A small robot carefully applies eyeliner with a thin brush in her bathroom." \
+  --checkpoint_folder ./outputs/myTrain/train_ucf101_ApplyEyeMakeup-short \
   --checkpoint_index 400 \
-  --noise_prior 0.3 \
+  --noise_prior 0.5 \
   --seed 785490 \
   --num-frames 24 \
   --num-steps 100 \
   --width 256 \
   --height 192 \
   --guidance-scale 12 \
-  --output_dir ./outputs/inference/HandstandWalking/noise0.3 \
